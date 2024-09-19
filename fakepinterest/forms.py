10894e5a032497fe42525f3e63c70d6527a76ebf
Rafael-Domiciano-Maia-Mateus <rmaia7985@gmail.com>
2024-09-18 21:02:47 -0300

    first commit

---
+++ fakepinterest/forms.py
@@ -0,0 +1,1 @@
+# Criar os formularios do nosso site
---
+++ fakepinterest/models.py
@@ -0,0 +1,18 @@
+# Criar estrutura do banco de dados
+from enum import unique
+
+from fakepinterest import database
+
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
+    fotos = database.relationship()
+
+
+class Foto(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    imagem = database.Column()
+    data_criacao = database.Column()
+    id_usuario = database.Column()
