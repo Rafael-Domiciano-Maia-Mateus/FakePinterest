c2e0ccb6f1ad74340b7548ac969ca6568da14707
Rafael <rmaia7985@gmail.com>
2024-09-21 23:07:09 -0300

    routes.py

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,14 +1,18 @@
-# Criar as rotas do nosso site (os links)
 from flask import render_template, url_for
 from fakepinterest import app
 from flask_login import login_required
+from fakepinterest.models import Usuario
+from fakepinterest import login_manager
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-
 @app.route("/perfil/<usuario>")
 @login_required
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
+
+@login_manager.user_loader
+def load_user(id_usuario):
+    return Usuario.query.get(int(id_usuario))
