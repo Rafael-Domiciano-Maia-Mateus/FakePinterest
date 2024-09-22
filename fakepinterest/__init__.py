ebb942d898b36d48c579b8cccdce2462d6a6d6f7
Rafael <rmaia7985@gmail.com>
2024-09-21 22:38:34 -0300

    __init__.py

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "863a1898d904e609474c0b1d1e07d924"
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
 
 from fakepinterest import routes
