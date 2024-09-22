4f5f3aea62094c8a17cc17c3407b28aa4ee5c0bc
Rafael <rmaia7985@gmail.com>
2024-09-22 16:06:42 -0300

    __init__.py

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "863a1898d904e609474c0b1d1e07d924"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
